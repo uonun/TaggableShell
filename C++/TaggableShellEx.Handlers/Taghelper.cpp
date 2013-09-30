c5df64d210992cccb230038c8aae0254649d3225
uonun <uonun@udnz.com>
2013-09-30 13:54:57 +0800

    Bug fixed:
    - Displaying the tags in right way now, and in max length of TAG_LENGTH.
    - Supporting special characters in tags.

--- C++/TaggableShellEx.Handlers/Taghelper.cpp
+++ C++/TaggableShellEx.Handlers/Taghelper.cpp
@@ -1,6 +1,5 @@
 #include "Taghelper.h"
 
-
 CTaghelper::CTaghelper(void)
 {
 
@@ -11,103 +10,41 @@ CTaghelper::~CTaghelper(void)
 {
 }
 
-void CTaghelper::GetTags(LPWSTR* tags,int* count){
-	//std::locale utf8_locale(std::locale(), new gel::stdx::utf8cvt<true>);
-	//std::wfstream fs;
-	//fs.imbue(utf8_locale);
-	//fs.open(yourfile, mode);
-
-
-	//*count=0;
-	//wfstream file;
-	//file.open(g_TagsFullName,ios::in);
-	//if(!file.fail())
-	//{
-	//int maxloop = 1000;
-	//while(file.peek() != EOF && !file.eof() && maxloop-- >0)
-	//	{
-	//		tags[*count] = new wchar_t[TAG_LENGTH];
-	//		memset(tags[*count],0,TAG_LENGTH * sizeof(wchar_t));
-	//		file.getline(tags[*count],TAG_LENGTH,'\n');
-
-	//		if(tags[*count][0]!='\0'){
-	//			(*count)++;
-
-	//			if((*count)>= MAXCOUNT_TAG)
-	//				return;
-	//		}
-	//	}
-	//}
-	//file.close();
-
-
-	*count=0;
+void CTaghelper::GetTags(LPWSTR* tags,int* count)
+{
+	*count = 0;
 	fstream file;
-	file.open(g_TagsFullName,ios::in);
-	if(!file.fail())
-	{
-		int maxloop = 1000;
-		while(file.peek() != EOF && !file.eof() && maxloop-- >0)
+	try	{
+		file.open(g_TagsFullName,ios::in,_SH_DENYNO);
+		if(!file.fail())
 		{
-			char tmp[TAG_LENGTH];
-			memset(tmp,0,TAG_LENGTH * sizeof(char));
-			file.getline(tmp,TAG_LENGTH,'\n');
+			int maxloop = 1000;
+			const int buffLen = 1024;
+			while(file.peek() != EOF && maxloop-- > 0)
+			{
+				char tmp[buffLen];
+				memset(tmp,0,sizeof(tmp) * sizeof(char));
+				file.getline(tmp,buffLen,'\n');
 
-			if(tmp[0]!='\0'){
-				tags[*count] = new wchar_t[TAG_LENGTH];
-				memset(tags[*count],0,TAG_LENGTH * sizeof(wchar_t));
+				if(tmp[0]!='\0'){
+					tags[*count] = new wchar_t[TAG_LENGTH];
+					memset(tags[*count],0,TAG_LENGTH * sizeof(wchar_t));
 
-				DWORD dwNum = MultiByteToWideChar(CP_UTF8,NULL,tmp,-1,NULL,0);
-				if(dwNum > 0){
-					MultiByteToWideChar(CP_UTF8,NULL,tmp,-1,tags[*count],dwNum > TAG_LENGTH? TAG_LENGTH : dwNum);
-					(*count)++;
+					DWORD dwNum = MultiByteToWideChar(CP_UTF8,NULL,tmp,-1,NULL,0);
+					if(dwNum > 0){
+						MultiByteToWideChar(CP_UTF8,NULL,tmp,-1,tags[*count],TAG_LENGTH);
+						tags[*count][TAG_LENGTH] = '\0'; // end the string. in case of the string "tmp" is null-terminated.
+						(*count)++;
+					}
+
+					if((*count)>= MAXCOUNT_TAG)
+						break;
 				}
-
-				if((*count)>= MAXCOUNT_TAG)
-					return;
 			}
 		}
+		file.close();
 	}
-	file.close();
-
-
-	/*
-	ifstream file; 
-	int LINES;
-	file.open(g_TagsFullName,ios::in);
-	if(file.fail())
-	{
-	cout<<"文件不存在."<<endl;
-	file.close();
-	cin.get();
-	cin.get();
+	catch(int& value){
+		file.close();
 	}
-	else//文件存在
-	{
-	#pragma region CountLines
-	int n=0;
-	string temp;
-	while(getline(file,temp))
-	{
-	n++;
-	}
-	LINES= n;
-	#pragma endregion
-
-	int *tc=new int[LINES]; 
-	char *t=new char[LINES]; 
-	int i=0; 
-	while(!file.eof()) //读取数据到数组
-	{ 
-	file>>tc[i]; 
-	file>>t[i]; 
-	i++; 
-	} 
-	file.close(); //关闭文件
-	for(i=0;i<LINES;i++)//输出数组内容
-	cout<<tc[i]<<"\t"<<t[i]<<endl;
-	cin.get();
-	cin.get();
-	}
-	*/
 }
