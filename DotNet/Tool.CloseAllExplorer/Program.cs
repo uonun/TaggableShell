9c3eaabe59b6dce10d533cbcaa108ed7f9f31672
uonun <uonun@udnz.com>
2013-10-14 13:51:08 +0800

    add tool for reset all processes of "exploer.exe"

---
+++ DotNet/Tool.CloseAllExplorer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Tool.CloseAllExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = Process.GetProcessesByName("explorer");
+            foreach (var item in list)
+            {
+                item.Kill();
+            }
+        }
+    }
+}
